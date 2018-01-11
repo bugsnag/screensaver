2bcd47c32f6ccf8f10b22e22bde5b28a9a00795c
Christian Schlensker <christian@cswebartisan.com>
2018-01-10 20:10:19 -0800

    Initial Commit

---
+++ Bugsaver/BugsaverView.h
@@ -0,0 +1,13 @@
+//
+//  BugsaverView.h
+//  Bugsaver
+//
+//  Created by Christian Schlensker on 1/10/18.
+//  Copyright © 2018 Bugsnag. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface BugsaverView : ScreenSaverView
+
+@end
